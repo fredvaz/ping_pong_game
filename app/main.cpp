bd3d42137c6b486fee71d150d24766bff8d40eaa
fredvaz <fredvaz8@gmail.com>
2023-03-20 16:53:27 +0000

    CMake config init

---
+++ app/main.cpp
@@ -0,0 +1,23 @@
+/*
+
+Ping Pong Game:
+
+General Notes:
+
+   [REF]:
+
+*/
+#include <iostream>
+#include <SFML/Graphics.hpp>
+
+//
+
+int main()
+{
+
+    std::cout << "Hello World!\n";
+
+    
+
+    return 0;
+}
